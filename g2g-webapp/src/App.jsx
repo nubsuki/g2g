d88f7167f7c865b0971de69def1e8c0dc1c2747e
nubsuki <kavinduherath.14@gmail.com>
2025-07-25 15:39:01 +0530

    Updated

--- g2g-webapp/src/App.jsx
+++ g2g-webapp/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import LoginForm from './Components/LoginForm'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <LoginForm/>
     </div>
   )
 }
--- g2g-webapp/src/Components/Navbar.jsx
+++ g2g-webapp/src/Components/Navbar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Navbar.css'
-import g2gLogo from '../assets/g2glogo2.png'
+import g2gLogo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
     <header className='header'>
-      <a href='#' className='logo'>
-        <img src={g2gLogo} alt="G2G Logo" />From Gamers To Gamers
-      </a>
+      <a href='#' className='logo'>G2G</a>
       <nav className='navbar'>
         <a href='#'>Home</a>
         <a href='#'>About</a>
